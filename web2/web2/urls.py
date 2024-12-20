2c5b545f7ad3a7954151fa99416b7482b86a54c7
HoangKiew <hoangmaikieu1@gmail.com>
2024-12-20 16:19:52 +0700

    commit2

--- web2/web2/urls.py
+++ web2/web2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for web1 project.
+URL configuration for web2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home import views as home  
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home.get_home)
 ]
