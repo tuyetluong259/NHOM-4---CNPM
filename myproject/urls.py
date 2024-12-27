e2acdf2be72bd95566b979b2c9bf84edfa9d896d
KieuTranThuUyen <uyen893605@gmail.com>
2024-12-28 00:22:25 +0700

    Add staff directory

--- myproject/urls.py
+++
@@ -1,8 +0,0 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('staff/', include('staff.urls')),  # Bao gồm các URL của app staff
-]
