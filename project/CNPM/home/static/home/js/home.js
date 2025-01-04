369995eb6eddef0095f5ed9552b78893714603d0
tuyetluong259 <luongtuyet.peter@gmail.com>
2025-01-04 23:01:32 +0700

    update13

--- project/CNPM/home/static/home/js/home.js
+++ project/CNPM/home/static/home/js/home.js
@@ -10,3 +10,35 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+// Open the full screen search box
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  // Close the full screen search box
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
+  function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-green";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-green", "");
+    }
+  }
+  
+  function myDropFunc() {
+    var x = document.getElementById("demoDrop");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-green";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-green", "");
+    }
+  }
