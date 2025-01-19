d1e6bc7a332a1e2447e6f4126901e6314015e214
unknown <thaonguyen17012005@gmail.com>
2025-01-19 23:58:24 +0700

    booking1

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = 'home'),
+    path('booking/', views.booking, name='booking'),
+    path('pet/', views.pet, name='pet'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('room/', views.room, name='room'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'app/home.html')
+def booking(request):
+    return render(request, 'app/booking.html')
+def schedule(request):
+    return render(request, 'app/schedule.html')
+def pet(request):
+    return render(request, 'app/pet.html')
+def room(request):
+    return render(request, 'app/room.html')
